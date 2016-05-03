aa9920f4271ddc1bf73c102d453a2c726a0aedd2
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-05-03 21:43:02 +0200

    Added boilerplate for backbone projects

---
+++ backboneBoilerplate/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    console.log("klar");
+})
+// In the first few sections, we do all the coding here.
+// Later, you'll see how to organize your code into separate
+// files and modules.
