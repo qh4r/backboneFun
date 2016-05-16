ad3bdc7a4ae65136e06ebff2f9380928e8f06d65
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-05-17 01:14:09 +0200

    Added toggle to model elements

--- secondProject/js/models/todoElement.js
+++ secondProject/js/models/todoElement.js
@@ -1,7 +1,13 @@
 var TodoElement = Backbone.Model.extend({
+    defaults: {
+        isComplete: false
+    },
     validate: function(input){
         if(!input || !input.description){
             throw new Error('no decription passed');
         }
+    },
+    toggleCompletion: function(){
+        this.set('isComplete', !this.get('isComplete'));
     }
 });
--- secondProject/js/views/todoElementView.js
+++ secondProject/js/views/todoElementView.js
@@ -5,12 +5,23 @@ var TodoElementView = Backbone.View.extend({
             throw new Error('no model passed');
         }
         this.model = data.model;
+        this.model.on('change', this.render, this);
+    },
+    events: {
+        'click input[type=checkbox]:first': 'onToggle'
+    },
+
+    onToggle: function(){
+        this.model.toggleCompletion();
     },
 
    render: function(){
+       //DRUGI ARGUMENT SPRAWIA ZE TOGGLE USTAWIA ZGODNIE Z WARTOSCIA BOOLA
+       this.$el.toggleClass('complete', this.model.get('isComplete'));
+
        //ESCAPE ZAMIAST GET SPRAWIA ZE HTML NIE BEDZIE WYKONYWAY
        //UNIKA CROSS SIDE SCRIPTINGU
-       this.$el.html(this.model.escape('description'));
+       this.$el.html('<input type="checkbox" '+(this.model.get("isComplete") ? 'checked' : '')+ '> '+ this.model.escape('description'));
 
        return this;
    }
